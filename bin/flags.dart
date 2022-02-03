ed7e6a24526e610ea65c139431c548cf83c43dd2
Jakob Kordež <jakobkordez1999@gmail.com>
2022-02-03 17:07:52 +0100

    Add flags

---
+++ bin/flags.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+import 'package:ham_tools/src/utils/callsign_util.dart';
+
+const res = '64';
+const uri = 'https://static.qrz.com/static/flags-iso/flat/$res/';
+
+Future<void> main(List<String> args) async {
+  final path = Platform.script.resolve('../assets/flags/$res').toFilePath();
+  final flags = Set.of(getFlags());
+
+  for (final flag in flags) {
+    final request = await HttpClient().getUrl(Uri.parse('$uri$flag.png'));
+    final response = await request.close();
+    await response.pipe(File('$path/$flag.png').openWrite());
+  }
+}
+
+Iterable<String> getFlags([List<DxccEntity> dxccs = DxccEntity.dxccs]) sync* {
+  for (final e in dxccs) {
+    yield e.flag;
+    yield* getFlags(e.sub);
+  }
+}
